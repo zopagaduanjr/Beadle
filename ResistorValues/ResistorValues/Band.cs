0732d730bdcaae15cf65fd78e8423d7c772b2538
Zaldy Jr Pagaduan <zaldypagaduanjr@outlook.com>
2018-12-11 12:39:38 +0800

    Practiced MVVM with xamarin.forms
    Made dummy app that calculates resistor value

---
+++ ResistorValues/ResistorValues/Band.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResistorValues
+{
+    public class Band
+    {
+        private string Color;
+        private int value;
+
+        public Band()
+        {
+            
+        }
+
+    }
+}
